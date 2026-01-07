0a5ac1448154c649f7a60e46501ffaa01a73e492
vikrin-creator <vikrin48@gmail.com>
2026-01-07 18:17:13 +0530

    Added backend logic and implemented the JWT authentication

--- backend/config/database.php
+++ backend/config/database.php
@@ -2,7 +2,7 @@
 
 class Database {
     private static $instance = null;
-    private $host = 'srv2124.hstgr.io';
+    private $host = 'localhost';
     private $db_name = 'u177524058_YATHA';
     private $username = 'u177524058_YATHA';
     private $password = 'Yatha@2025';
